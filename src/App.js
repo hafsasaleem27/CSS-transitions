504239ff06475239a0b7e8448937e90e888134b4
Hafsa Saleem <hafsa.saleem27@gmail.com>
2023-06-28 16:03:03 +0500

    portaled modal & backdrop to their divs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
 import "./App.css";
 import Modal from "./components/Modal/Modal";
@@ -6,29 +7,38 @@ import Backdrop from "./components/Backdrop/Backdrop";
 import List from "./components/List/List";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
       isModalOpen: false,
     };
-  };
+  }
 
   showModal = () => {
     this.setState({ isModalOpen: true });
-  }
+  };
 
   closeModal = () => {
     this.setState({ isModalOpen: false });
-  }
+  };
 
   render() {
     return (
       <div className="App">
         <h1>React Animations</h1>
-        <Modal show={this.state.isModalOpen} closed={this.closeModal}/>
-        <Backdrop show={this.state.isModalOpen} closed={this.closeModal}/>
-        <button className="Button" onClick={this.showModal}>Open Modal</button>
+        {/* <Modal show={this.state.isModalOpen} closed={this.closeModal} /> */}
+        {/* <Backdrop show={this.state.isModalOpen} closed={this.closeModal} /> */}
+        {ReactDOM.createPortal(
+          <Modal show={this.state.isModalOpen} closed={this.closeModal} />,
+          document.getElementById("modal-root")
+        )}
+        {ReactDOM.createPortal(
+          <Backdrop show={this.state.isModalOpen} closed={this.closeModal} />,
+          document.getElementById("backdrop-root")
+        )}
+        <button className="Button" onClick={this.showModal}>
+          Open Modal
+        </button>
         <h3>Animating Lists</h3>
         <List />
       </div>
